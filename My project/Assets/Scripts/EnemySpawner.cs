140c6ee25305a308593d8bb659dc465e9d6defca
timasharish08 <81697635+timasharish08@users.noreply.github.com>
2023-08-22 19:28:26 +0300

    first commit

--- My project/Assets/Scripts/EnemySpawner.cs
+++ My project/Assets/Scripts/EnemySpawner.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+using System.Collections;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -7,24 +7,22 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private EnemyTarget _enemyTarget;
 
     [SerializeField] private float _spawnTime;
-    private float _currentTime;
 
-    private void Update()
+    private void Start()
     {
-        if (_currentTime > 0)
-        {
-            _currentTime -= Time.deltaTime;
-        }
-        else
-        {
-            Spawn();
-            _currentTime = _spawnTime;
-        }
+        StartCoroutine(Spawning());
     }
 
-    private void Spawn()
+    private IEnumerator Spawning()
     {
-        Enemy enemy = Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
-        enemy.Init(_enemyTarget);
+        bool isSpawning = true;
+        WaitForSeconds wait = new WaitForSeconds(_spawnTime);
+
+        while (isSpawning)
+        {
+            Enemy enemy = Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+            enemy.Init(_enemyTarget);
+            yield return wait;
+        }
     }
 }
